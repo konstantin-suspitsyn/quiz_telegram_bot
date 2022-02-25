0634c448c3687a48b5e1a78540fa4bf6075a8cbd
konstantin_s <konstantin.suspitsyn@gmail.com>
2022-02-25 11:50:20 +0300

    CSTB0: Added SpringBoot

---
+++ src/test/java/com/github/konstantinsuspitsyn/customsurveytelegrambot/CustomSurveyTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.konstantinsuspitsyn.customsurveytelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSurveyTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
